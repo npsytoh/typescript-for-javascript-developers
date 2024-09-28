3ea0bacc2a0487d6ce64b9bf8ba88468e0044d6c
npsytoh <npsytoh@gmail.com>
2024-09-28 23:41:22 +0900

    add interfaces.ts

---
+++ src/interfaces.ts
@@ -0,0 +1,16 @@
+export {};
+
+type ObjectType = {
+	name: string;
+	age: number;
+};
+
+interface ObjectInterface {
+	name: string;
+	age: number;
+}
+
+let example: ObjectInterface = {
+	name: 'Ken',
+	age: 25,
+};
