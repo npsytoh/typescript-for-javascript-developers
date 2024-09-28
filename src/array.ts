faa9d6002614a1bc5a88ec5425cd59b48f5c88de
npsytoh <npsytoh@gmail.com>
2024-09-28 18:10:00 +0900

    add array.ts

---
+++ src/array.ts
@@ -0,0 +1,16 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+console.log({ numbers });
+
+let numbers2: Array<number> = [1, 2, 3];
+let strings2: Array<string> = ['tokyo', 'osaka', 'kyoto'];
+
+let strings: string[] = ['typescript', 'javascript'];
+
+let array2d: number[][] = [
+	[50, 100],
+	[150, 300],
+];
+
+let sumArray: (string | number | boolean)[] = [1, false, 'japan'];
