38e476f4a5b5444c65ddfff92bcc0902f100086e
npsytoh <npsytoh@gmail.com>
2024-09-29 14:42:58 +0900

    add enum.ts

---
+++ src/enum.ts
@@ -0,0 +1,33 @@
+export {};
+
+enum Months {
+	January = 1,
+	February,
+	March,
+	April,
+	May,
+	Jun,
+	July,
+	August,
+	September,
+	October,
+	November,
+	December,
+}
+
+console.log(Months.December);
+
+enum COLORS {
+	RED = '#FF0000',
+	WHITE = '#FFFFFF',
+	GREEN = '#008000',
+	BLUE = '#0000FF',
+	BLACK = '#000000',
+}
+
+let GREEN = COLORS.GREEN;
+console.log({ GREEN });
+
+enum COLORS {
+	YELLOW = '#FFFF00',
+}
