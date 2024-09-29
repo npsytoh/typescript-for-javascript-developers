a956e81850ce94c50d558138d11832d8cfc0785a
npsytoh <npsytoh@gmail.com>
2024-09-29 14:25:29 +0900

    add literal.ts

---
+++ src/literal.ts
@@ -0,0 +1,12 @@
+export {};
+
+type WeekDays = '日' | '月' | '火' | '水' | '木' | '金' | '土';
+let dayOfTheWeek: WeekDays = '日';
+dayOfTheWeek = '月';
+
+type Month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+let month: Month = 1;
+month = 12;
+
+let TRUE: true = true;
+// TRUE = false;
