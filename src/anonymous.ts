785b8e2c1e07a6abdc6db72c7c201de16169375b
npsytoh <npsytoh@gmail.com>
2024-09-29 15:21:17 +0900

    add anonymous.ts

---
+++ src/anonymous.ts
@@ -0,0 +1,7 @@
+export {};
+
+const bmi: (height: number, weight: number) => number = function (height: number, weight: number): number {
+	return weight / height ** 2;
+};
+
+console.log(bmi(1.65, 45));
